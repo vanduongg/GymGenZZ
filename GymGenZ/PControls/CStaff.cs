109a2c268d997bf784b67c06c2d43cd5d2fcf87d
Hau2223 <nguyentrunghau220203@gmail.com>
2023-12-03 20:21:17 +0700

    Update code of Manager Staff

--- GymGenZ/PControls/CStaff.cs
+++ GymGenZ/PControls/CStaff.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using GymGenZ.PModels;
 using System.Data.SqlClient;
+using System.Collections;
+using System.Drawing;
 using System.Windows.Forms;
 
 namespace GymGenZ.PControls
@@ -18,7 +20,6 @@ namespace GymGenZ.PControls
         public CStaff(string connectionString)
         {
             _conn = new SQLiteConnection(connectionString);
-            _conn.Open();
         }
 
         public bool Login(string username, string password)
@@ -103,6 +104,149 @@ namespace GymGenZ.PControls
                     return true;
                 }
             }
+        }        
+        public List<MStaff> SearchStaff(string searchText)
+        {
+            List<MStaff> staffs = new List<MStaff>();
+
+            using (SQLiteConnection con = new SQLiteConnection(_conn))
+            {
+                con.Open();
+                string query = "SELECT Staff.id AS StaffID, Staff.username AS UserName, " +
+                               "Staff.roll AS Roll, Staff.fullName AS FullName, " +
+                               "Staff.numberPhone AS NumberPhone, Staff.idCard AS IDCard, " +
+                               "Staff.gender AS Gender, Staff.birth AS Birth, Staff.address AS Address " +
+                               "FROM Staff " +
+                               "WHERE Staff.username LIKE @searchText OR Staff.idCard LIKE @searchText OR " +
+                               "Staff.fullName LIKE @searchText OR Staff.numberPhone LIKE @searchText";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@searchText", $"%{searchText}%");
+                    using (SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            MStaff staff = new MStaff
+                            {
+                                StaffID = reader["StaffID"].ToString(),
+                                username = reader["UserName"].ToString(),
+                                roll = reader["Roll"].ToString(),
+                                fullname = reader["FullName"].ToString(),
+                                numberPhone = reader["NumberPhone"].ToString(),
+                                idCard = reader["IDCard"].ToString(),
+                                gender = reader["Gender"].ToString(),
+                                birth = reader["Birth"].ToString(),
+                                address = reader["Address"].ToString()
+                            };
+                            staffs.Add(staff);
+                        }
+                    }
+                }
+            }
+            return staffs;
         }
+
+        public bool CheckPhoneNumber(string numberPhone)
+        {
+            using (SQLiteConnection con = new SQLiteConnection(_conn))
+            {
+                con.Open();
+                string query = "SELECT COUNT(*) FROM Staff WHERE numberPhone = @numberPhone";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@numberPhone", numberPhone);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return count > 0;
+                }
+            }
+        }
+
+        public bool CheckIDCard(string idCard){
+            using (SQLiteConnection con = new SQLiteConnection(_conn)){
+                con.Open();
+                string query = "SELECT COUNT(*) FROM Staff WHERE idCard = @idCard";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@idCard", idCard);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return count > 0;
+                }
+            }
+        }
+        public bool CheckUsername(string username){
+            using (SQLiteConnection con = new SQLiteConnection(_conn)){
+                con.Open();
+                string query = "SELECT COUNT(*) FROM Staff WHERE username = @username";
+                using (SQLiteCommand cmd = new SQLiteCommand(query, con)){
+                    cmd.Parameters.AddWithValue("@username", username);
+                    int count = Convert.ToInt32(cmd.ExecuteScalar());
+                    return count > 0;
+                }
+            }
+        }
+        public bool AddStaff(MStaff newStaff)
+        {
+            using (SQLiteConnection con = new SQLiteConnection(_conn))
+            {
+                string insertQuery = "INSERT INTO Staff (username, password, roll, fullName, numberPhone, idCard, gender, birth, address) " +
+                                     "VALUES (@username, @password, @roll, @fullName, @numberPhone, @idCard, @gender, @birth, @address)";
+                using (SQLiteCommand insertCmd = new SQLiteCommand(insertQuery, con)){
+                    insertCmd.Parameters.AddWithValue("@username", newStaff.username);
+                    insertCmd.Parameters.AddWithValue("@password", newStaff.password);
+                    insertCmd.Parameters.AddWithValue("@roll", newStaff.roll);
+                    insertCmd.Parameters.AddWithValue("@fullName", newStaff.fullname);
+                    insertCmd.Parameters.AddWithValue("@numberPhone", newStaff.numberPhone);
+                    insertCmd.Parameters.AddWithValue("@idCard", newStaff.idCard);
+                    insertCmd.Parameters.AddWithValue("@gender", newStaff.gender);
+                    insertCmd.Parameters.AddWithValue("@birth", newStaff.birth);
+                    insertCmd.Parameters.AddWithValue("@address", newStaff.address);
+                    con.Open();
+                    int rowsAffected = insertCmd.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+        }
+        public bool UpdateStaff(MStaff updatedStaff)
+        {
+            using (SQLiteConnection con = new SQLiteConnection(_conn))
+            {
+                string updateQuery = "UPDATE Staff SET username = @username, " +
+                                     "roll = @roll, fullName = @fullName, numberPhone = @numberPhone, " +
+                                     "idCard = @idCard, gender = @gender, birth = @birth, address = @address WHERE ID = @staffID";
+                using (SQLiteCommand updateCmd = new SQLiteCommand(updateQuery, con))
+                {
+                    updateCmd.Parameters.AddWithValue("@username", updatedStaff.username);
+                    updateCmd.Parameters.AddWithValue("@password", updatedStaff.password);
+                    updateCmd.Parameters.AddWithValue("@roll", updatedStaff.roll);
+                    updateCmd.Parameters.AddWithValue("@fullName", updatedStaff.fullname);
+                    updateCmd.Parameters.AddWithValue("@numberPhone", updatedStaff.numberPhone);
+                    updateCmd.Parameters.AddWithValue("@idCard", updatedStaff.idCard);
+                    updateCmd.Parameters.AddWithValue("@staffID", updatedStaff.StaffID);
+                    updateCmd.Parameters.AddWithValue("@gender", updatedStaff.gender);
+                    updateCmd.Parameters.AddWithValue("@birth", updatedStaff.birth);
+                    updateCmd.Parameters.AddWithValue("@address", updatedStaff.address);
+                    con.Open();
+                    int rowsAffected = updateCmd.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+        }
+        public bool DeleteStaff(string staffID)
+        {
+            using (SQLiteConnection con = new SQLiteConnection(_conn))
+            {
+                string deleteQuery = "DELETE FROM Staff WHERE ID = @staffID";
+                using (SQLiteCommand deleteCmd = new SQLiteCommand(deleteQuery, con))
+                {
+                    deleteCmd.Parameters.AddWithValue("@staffID", staffID);
+
+                    con.Open();
+                    int rowsAffected = deleteCmd.ExecuteNonQuery();
+                    return rowsAffected > 0;
+                }
+            }
+        }
+
+
     }
 }
